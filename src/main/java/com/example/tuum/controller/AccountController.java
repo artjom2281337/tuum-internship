c8a6b8893f8be2eeafc7879adb55e1ed8b1b0324
Artjom Smorgulenko <artjomsmorgulenko@gmail.com>
2026-04-23 16:01:59 +0300

    AccountController created

---
+++ src/main/java/com/example/tuum/controller/AccountController.java
@@ -0,0 +1,45 @@
+package com.example.tuum.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.tuum.dto.AccountResponse;
+import com.example.tuum.dto.CreateAccountRequest;
+import com.example.tuum.service.AccountService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+    private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/{account_id}")
+    public ResponseEntity<AccountResponse> getAccount(@PathVariable Long account_id) {
+        logger.info("Received GetAccount request for id: {}", account_id);
+        AccountResponse response = accountService.getAccount(account_id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountResponse> createAccount(@RequestBody CreateAccountRequest request) {
+        logger.info("Received createAccount request for id: {}", request.getCustomer_id());
+        AccountResponse response = accountService.createAccount(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+        
+}
